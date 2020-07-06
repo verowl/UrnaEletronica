7f6058a9a98f7e8e551c0c16ae7765bf65dcbf42
verowl <alberto@albertomartins.com.br>
2020-07-05 22:36:29 -0300

    New Version

--- Presidente.py
+++ Presidente.py
@@ -1,5 +1,7 @@
-var senhaPresidente
+var senhaPresidente=""
 
-def abreUrna (senha)
-    if senha==senhaPresidente {
-}
+def abreUrna (senha):
+    if senha==senhaPresidente
+        print("Abertura da Urna Autorizada")
+    else
+        print("Abertura da Urna Nao Autorizada")
